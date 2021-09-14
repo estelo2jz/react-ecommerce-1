4ec8df536fb55fbaa54df2ff388a1242e82d3b58
esteloabell <manitobaaa@gmail.com>
2021-09-14 09:49:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // import Home from './pages/Home';
 // import ProductList from './pages/ProductList';
+// import Product from "./pages/Product";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
 
-import Product from "./pages/Product";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div className="App">
-      <Product />
+      <Cart />
     </div>
   );
 }
